306e22850c3661f86d39aa11806051d1f0f6d390
Fatima <Fatimaoutarrast@gmail.com>
2024-12-06 21:38:41 +0100

    Graphbuild completed

---
+++ client/src/pages/dashboard/AI/index.js
@@ -0,0 +1,19 @@
+import TextGenerate from "./TextGenerate";
+
+import GenerateFill from "./cloudinary/GenerateFill";
+import BgRemover from "./cloudinary/BgRemover";
+import Restore from "./cloudinary/Restore";
+
+import Gallery from "./cloudinary/Gallery";
+
+/* Model */
+import Model from "./Model";
+
+export {
+  TextGenerate,
+  GenerateFill,
+  Gallery,
+  Model,
+  Restore,
+  BgRemover,
+};
